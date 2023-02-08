7a4dc370faefc29252005fae8d8f1e5a64150c7b
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-08 13:18:41 -0300

    Criando erro para mostrar na pipeline CI

--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -1,5 +1,5 @@
 it.only('testa a página da política de privacidade de forma independente', ()=> {
     cy.visit('../../src/privacy.html')
     cy.get('#title').should('contain','CAC TAT - Política de privacidade') 
-    cy.contains('Talking About Testing').should('be.visible')   
+    cy.contains('Talking About esting').should('be.visible')   
 })
